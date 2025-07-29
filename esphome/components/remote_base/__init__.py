8848dbc97f4cb4d0afebd983272bc6f3a6512f22
Erik Hughes <erik.hughes@outlook.com>
2025-07-29 18:16:50 +0200

    feat: add lucci air protocol

--- esphome/components/remote_base/__init__.py
+++ esphome/components/remote_base/__init__.py
@@ -2064,3 +2064,66 @@ async def housegard_origo_action(var, config, args):
     cg.add(var.set_pairing_key(template_))
     template_ = await cg.templatable(config[CONF_IS_PAIRING], args, cg.bool_)
     cg.add(var.set_is_pairing(template_))
+
+
+# Lucci Air
+(
+    LucciAirData,
+    LucciAirBinarySensor,
+    LucciAirTrigger,
+    LucciAirAction,
+    LucciAirDumper,
+) = declare_protocol("LucciAir")
+
+CONF_DEVICE_ID = "device_id"
+
+LUCCI_AIR_COMMANDS = [
+    "direction",
+    "speed_1", 
+    "speed_2",
+    "speed_3", 
+    "speed_4",
+    "speed_5",
+    "speed_6",
+    "power",
+    "timer_1h",
+    "timer_4h", 
+    "timer_8h",
+    "light",
+    "speed_cycle",
+    "away",
+]
+
+LUCCI_AIR_SCHEMA = cv.Schema(
+    {
+        cv.Required(CONF_COMMAND): cv.one_of(*LUCCI_AIR_COMMANDS, lower=True),
+        cv.Required(CONF_DEVICE_ID): cv.hex_uint64_t,
+    }
+)
+
+@register_binary_sensor("lucci_air", LucciAirBinarySensor, LUCCI_AIR_SCHEMA)
+def lucci_air_binary_sensor(var, config):
+    cg.add(
+        var.set_data(
+            cg.StructInitializer(
+                LucciAirData,
+                ("command", config[CONF_COMMAND]),
+                ("device_id", config[CONF_DEVICE_ID]),
+            )
+        )
+    )
+
+@register_trigger("lucci_air", LucciAirTrigger, LucciAirData)
+def lucci_air_trigger(var, config):
+    pass
+
+@register_dumper("lucci_air", LucciAirDumper)
+def lucci_air_dumper(var, config):
+    pass
+
+@register_action("lucci_air", LucciAirAction, LUCCI_AIR_SCHEMA)
+async def lucci_air_action(var, config, args):
+    template_ = await cg.templatable(config[CONF_COMMAND], args, cg.std_string)
+    cg.add(var.set_command(template_))
+    template_ = await cg.templatable(config[CONF_DEVICE_ID], args, cg.uint64)
+    cg.add(var.set_device_id(template_))
